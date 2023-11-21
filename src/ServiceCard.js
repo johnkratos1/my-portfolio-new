c383b75a91aca52d21a595b1f3b8257425783438
johnkratos1 <113154336+johnkratos1@users.noreply.github.com>
2023-11-21 07:04:06 +0100

    major adjstments
    I did some stuffs

--- src/ServiceCard.js
+++ src/ServiceCard.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 
-function ServiceCard({src, title, description, intro, list1, list2, list3, list4}) {
+function ServiceCard({
+  src,
+  title,
+  description,
+  intro,
+  list1,
+  list2,
+  list3,
+  list4,
+}) {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className="flex-col text-center border-double border-4 border-teal-500 shadow-black p-10 rounded-2xl my-10 dark:bg-white">
-    <Image src={src} width={250} height={200} className="mx-auto" />
-    <h3 className="text-lg font-medium pt-8 pb-2">
-      {title}
-    </h3>
-    <p className="py-2">
-      {description}
-    </p>
-    <h4 className="text-teal-600 py-4">{intro}</h4>
-    <p className="text-gray-800 py-1">{list1}</p>
-    <p className="text-gray-800 py-1">{list2}</p>
-    <p className="text-gray-800 py-1">{list3}</p>
-    <p className="text-gray-800 py-1">{list4}</p>
-  </div>
-  )
+    <div className={darkMode ? "dark" : ""}>
+      <div className="flex-col text-center border-double border-2 border-teal-500 shadow-black p-10 rounded-2xl my-10 dark:border-teal-600 dark:border-2 dark:text-white dark:rounded-lg lg:border-0 lg:border-gray-900 ">
+        <Image src={src} width={250} height={200} className="mx-auto" />
+        <h3 className="text-lg font-medium pt-8 text-teal-500">{title}</h3>
+        <p className="py-2 text-lg font-thin">{description}</p>
+        <h4 className="text-teal-600 py-4">{intro}</h4>
+        <p className="text-gray-800 py-1">{list1}</p>
+        <p className="text-gray-800 py-1">{list2}</p>
+        <p className="text-gray-800 py-1">{list3}</p>
+        <p className="text-gray-800 py-1">{list4}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ServiceCard
+export default ServiceCard;
--- src/app/page.js
+++ src/app/page.js
@@ -25,9 +25,9 @@ export default function Home() {
 
   return (
     <main className={darkMode ? "dark" : ""}>
-      <div className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
-        <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+      <div className="flex flex-col bg-white px-[5px] md:px-20 lg:px-0 dark:bg-gray-900 w-[100%]">
+        <section className="mb-16">
+          <nav className="py-4 flex justify-between lg:px-5 px-2 mx-[2px]">
             <h1 className="text-2xl font-burtons text-teal-950 italic overline dark:text-teal-100">
               Web Developer
             </h1>
@@ -40,7 +40,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:text-black"
                   href="#"
                 >
                   Resume
@@ -48,17 +48,19 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
+          <div className="text-center mt-4">
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl font-serif">
               John Ayomide Ezekiel
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl font-burtons dark:text-white">Front-End Developer</h3>
+            <h3 className="text-2xl py-2 md:text-3xl font-burtons dark:text-white">
+              Front-End Developer
+            </h3>
             <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto font-mono dark:text-gray-200">
               Freelancer providing services for programming and needs. Join me
               down and let's get cracking!
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+          <div className="text-5xl flex justify-between w-3/4 px-5 mx-auto mt-3">
             <a href="https://twitter.com/JohnAyo72850989">
               <AiFillTwitterCircle className="text-blue-400" />
             </a>
@@ -72,31 +74,32 @@ export default function Home() {
               <AiFillGithub className="text-neutral-800 dark:text-white" />
             </a>
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden mb-10 md:h-96 md:w-96">
+          {/* <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden mb-10 md:h-96 md:w-96 border-red-500 border-2">
             <Image src={pic} fill cover className="pr-5" />
-          </div>
+          </div> */}
         </section>
         {/* This is another section */}
-        <section>
-          <div className="p-5 rounded-md dark:bg-white mb-10">
-            <h3 className="text-3xl py-1 text-teal-600">Services I offer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 font-serif">
-              Since the beginning of my journey as a freelance developer, I've
-              done remote work for{" "}
+        <section className="w-full lg:px-8 px-[4px]">
+          <div className="rounded-md dark:text-white dark:rounded-lg p-4 w-full lg:w-full mb-10">
+            <h2 className="text-3xl text-teal-600 font-bold italic">WHAT I DO </h2>
+            <p className="text-md py-5 leading-8 text-gray-800 font-serif dark:text-white">
+              Since the beginning of my journey as a <span className="text-teal-500">Self-taught</span> Developer, I've
+              done several remote work for{" "}
               <span className="text-teal-500">Clients</span> and collaborated
               with talented people to create{" "}
               <span className="text-teal-500"> Digital Products</span> for both
               business and consumer use.
             </p>
             <p text-md py-5 leading-8 text-gray-800>
-              I offer from a wide range of services, including{" "}
+              I offer a wide range of services which includes{" "}
               <span className="text-teal-500">Web Design and Development</span>,{" "}
               <span className="text-teal-500">Mobile App Development</span>,{" "}
               <span className="text-teal-500">Website Maintenance</span> and{" "}
-              <span className="text-teal-500">teaching</span>.
+              <span className="text-teal-500">Code Classes </span> for new and inspiring programmers.
             </p>
           </div>
-          <div className="lg:flex gap-10">
+          <div className="flex lg:flex-row flex-col lg:justify-evenly w-full mx-auto gap-4 px-[5px] mb-5 items-center lg:p-5">
+          <div className="lg:w-1/3 h-full">
             <ServiceCard
               src={design}
               title={"Beautiful Web Designs"}
@@ -107,7 +110,9 @@ export default function Home() {
               list3={"JAVASCRIPT"}
               list4={"React Js & Next Js"}
             />
+            </div>
 
+            <div className="lg:w-1/3">
             <ServiceCard
               src={code}
               title={"Elegant Mobile App Development"}
@@ -117,7 +122,9 @@ export default function Home() {
               intro={"Design Tools I use"}
               list1={"React Native"}
             />
+            </div>
 
+            <div className="lg:w-1/3">
             <ServiceCard
               src={manage}
               title={"We Manage Existing Web Sites"}
@@ -131,24 +138,26 @@ export default function Home() {
               list2={
                 "Code Review - Rewrite Legacy Codes to meet current Practices and Standard for better Performance"
               }
-            />
+            /> </div>
           </div>
         </section>
-        <section>
-          <div className="p-10 rounded-md dark:bg-white">
+        <section className="px-[2px] lg:px-8">
+          <div className="p-10 rounded-md flex flex-col justify-center items-center">
             <h3 className="text-3xl py-1 text-teal-400">Portfolio</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">
               I develop my skills through building and participating on real
               life <span className="text-teal-500">Projects </span>
-              by collaborated with talented people to create
+              by collaborating with talented developers to create
               <span className="text-teal-500"> Digital Products</span> for both
               business and consumer use.
             </p>
-            <h2 className="text-center text-2xl text-teal-600">
+            <h2 className="text-center lg:text-3xl text-xl text-teal-500 lg:mt-[4px]">
               List of Projects and Web Application
             </h2>
           </div>
         </section>
+
+        <section className="flex flex-col lg:px-32">
         <Projects
           src={movie}
           appName={"Movie App"}
@@ -173,6 +182,8 @@ export default function Home() {
           desc={"A Simple Calculator built using HTML, CSS and JavaScript"}
           link={"https://johnkratos1.github.io/Calculator-App/updated.html"}
         />
+        </section>
+
 
         <Footer />
       </div>
